5dca2240327f0de0c4b3318bc6d6f7a194b8af28
Ali Yousefi <alifordevelpment@gmail.com>
2023-04-12 12:37:32 +0430

    shown todos in ui

--- js/myScripts.js
+++ js/myScripts.js
@@ -11,6 +11,7 @@ import { addTodoAction } from "./Redux/actionCreators.js";
 
 const todoInput = document.querySelector(".todo-input");
 const todoBtn = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 //Create Reduce todo list ---------------------------------------
 const todoListReducer = (state = [], action) => {
@@ -26,6 +27,8 @@ const todoListReducer = (state = [], action) => {
       return newState;
     }
     case removeTodo: {
+      let newState = [...state];
+
       return state;
     }
     case doTodo: {
@@ -57,6 +60,27 @@ todoBtn.addEventListener("click", (event) => {
 
   store.dispatch(addTodoAction(newTodoTitle));
   const todos = store.getState();
+  shownTodos(todos);
   todoInput.value = "";
   console.log(todos);
 });
+
+const shownTodos = (todos) => {
+  todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    todoList.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="todo">
+      <li class="todo-item">${todo.title}</li>
+      <button class="complete-btn">
+        <i class="fas fa-check-circle"></i>
+      </button>
+      <button class="trash-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+    `
+    );
+  });
+};
